10e7ee4847d51eda2b6ba845ca8657da7073bc8e
Maxime Thirouin <m@moox.io>
2014-05-29 08:09:49 +0200

    Vasy je sens que travis va me souler avec le repo dans le repo...

--- tasks/publish.js
+++ tasks/publish.js
@@ -10,7 +10,7 @@ var paths = require("./paths")
 module.exports = function(){
   return gulp.src(paths.dist.public + "**/*")
     .pipe(ghPages({
-      remoteUrl : "git@github.com:putaindecode/website.git",
+      remoteUrl : "https://github.com:putaindecode/website.git",
       branch : "gh-pages",
       cacheDir : __dirname + "/../.publish"
     }))
