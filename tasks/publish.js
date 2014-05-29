0aa9a89a943f50a17d19c3f2062bb717806c3c6e
Maxime Thirouin <m@moox.io>
2014-05-29 08:42:15 +0200

    add who is commiting + debug gh_token

--- tasks/publish.js
+++ tasks/publish.js
@@ -9,6 +9,7 @@ var paths = require("./paths")
  * publish build in the gh-pages branch
  */
 module.exports = function(){
+  console.log(process.env.GH_TOKEN ? "GH_TOKEN is here" + process.env.GH_TOKEN.length : "well gh_token is not here")
   return gulp.src(paths.dist.public + "**/*")
     .pipe(ghPages({
       remoteUrl : "https://" + (process.env.GH_TOKEN ? process.env.GH_TOKEN + "@" : "") + "github.com/putaindecode/website.git",
